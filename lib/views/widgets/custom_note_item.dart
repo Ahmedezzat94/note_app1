153eff09d4a3698ebe25eaa1ea0f048b80e7dfa0
ahmedezzat <ahmedezzat185@gmail.com>
2023-05-09 15:07:03 +0300

    create customNoteIcon and change th font with poppins font

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container(
