8659a6a512437fc731d5e5046ca6b42eb2798a2e
ahmedezzat <ahmedezzat185@gmail.com>
2023-05-09 12:50:44 +0300

    create NotesView and NotesViewBody and customAppBar and customSearchIcon

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:note_app1/views/widgets/notes_view_body.dart';
 
 class NotesViews extends StatelessWidget {
   const NotesViews({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
+
