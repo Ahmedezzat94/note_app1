c77d3ebcb6633bce0a974e29bd61351b2bdd5e36
ahmedezzat <ahmedezzat185@gmail.com>
2023-05-09 23:52:48 +0300

    create floating action button and bottom modal sheet with border radius

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:note_app1/views/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app1/views/widgets/notes_view_body.dart';
 
 class NotesViews extends StatelessWidget {
@@ -7,9 +8,21 @@ class NotesViews extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return  Scaffold(
+      floatingActionButton:FloatingActionButton(
+        onPressed: (){
+          showModalBottomSheet(
+            shape:RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16)
+            ),
+              context: context, builder: (context) {
+            return const AddNoteBottomSheet();
+          });
+        },
+        child:const Icon(Icons.add),),
+      body: const NotesViewBody(),
     );
   }
 }
+
 
---
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      child: Center(
+        child: Text("wsssaaaaaa3"),
+      ),
+    );
+  }
+}
